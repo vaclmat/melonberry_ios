835dc390367226b95ee3fe42995ac4f8a2a70d2e
Vaclav Matousek <vaclmat@email.cz>
2021-11-24 11:47:29 +0100

    Initial Commit

---
+++ Melonberry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Melonberry
+//
+//  Created by Václav Matoušek on 24.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Melonberry/MelonberryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelonberryApp.swift
+//  Melonberry
+//
+//  Created by Václav Matoušek on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MelonberryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
